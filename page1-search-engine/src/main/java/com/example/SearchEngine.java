1c4e6a2de7baa3c2bd06e2f08505f3a251717e2e
briantwhelan <whelanb8@tcd.ie>
2023-11-15 10:45:07 +0000

    Add reading of LA Times to main SearchEngine

--- page1-search-engine/src/main/java/com/example/SearchEngine.java
+++ page1-search-engine/src/main/java/com/example/SearchEngine.java
@@ -1,17 +1,20 @@
 package com.example;
 
 import com.example.ft.FinancialTimesReader;
+import com.example.latimes.LosAngelesTimesReader;
 
 /**
  * SearchEngine indexes and searches documents.
  */
 public class SearchEngine {
   public static final String FT_PATH = "./data/ft";
+  public static final String LATIMES_PATH = "./data/latimes";
 
   /**
    * Main method for SearchEngine.
    */
   public static void main(String[] args) throws Exception {
     FinancialTimesReader.readDocuments(FT_PATH);
+    LosAngelesTimesReader.readDocuments(LATIMES_PATH);
   }
 }
