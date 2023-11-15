4594349e294a935127a7c07454c6f2d0241877af
briantwhelan <whelanb8@tcd.ie>
2023-11-15 16:45:20 +0000

    Add reading of Federal Register to main SearchEngine

--- page1-search-engine/src/main/java/com/example/SearchEngine.java
+++ page1-search-engine/src/main/java/com/example/SearchEngine.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import com.example.fr.FederalRegisterReader;
 import com.example.ft.FinancialTimesReader;
 import com.example.latimes.LosAngelesTimesReader;
 
@@ -9,6 +10,7 @@ import com.example.latimes.LosAngelesTimesReader;
 public class SearchEngine {
   public static final String FT_PATH = "./data/ft";
   public static final String LATIMES_PATH = "./data/latimes";
+  public static final String FR_PATH = "./data/fr94";
 
   /**
    * Main method for SearchEngine.
@@ -16,5 +18,6 @@ public class SearchEngine {
   public static void main(String[] args) throws Exception {
     FinancialTimesReader.readDocuments(FT_PATH);
     LosAngelesTimesReader.readDocuments(LATIMES_PATH);
+    FederalRegisterReader.readDocuments(FR_PATH);
   }
 }
