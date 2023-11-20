0f6ee2be3ad6aad8641b20cd4232facffaa5545f
briantwhelan <whelanb8@tcd.ie>
2023-11-20 23:55:38 +0000

    Add qrels file and script to run trec_eval on results

--- run.sh
+++ run.sh
@@ -1,1 +1,11 @@
+#!/bin/sh
+
+# Run search engine.
+echo "Running search engine..."
 (cd ./page1-search-engine && mvn clean install && java -jar ./target/page1-search-engine-1.0.jar)
+
+# Run trec_eval and save results to file.
+for file in ./page1-search-engine/results/*; do
+  	echo "Running trec_eval for $(basename $file) and storing results to ./page1-search-engine/evaluation/$(basename $file).txt..."
+    (./trec_eval-9.0.7/trec_eval -m official ./page1-search-engine/evaluation/qrels $file) > ./page1-search-engine/evaluation/$(basename $file).txt
+done
