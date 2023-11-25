3e2738b4da75a52961cb555bb4a0daf46c8c7293
Priya Nawal <47970747+priyaaa705@users.noreply.github.com>
2023-11-25 22:15:56 +0000

    Update GeneralizedCustomAnalyzer.java

--- GeneralizedCustomAnalyzer.java
+++ GeneralizedCustomAnalyzer.java
@@ -1,22 +1,5 @@
 package com.example;
 
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import java.io.Reader;
 import java.util.Arrays;
 import java.util.List;
@@ -37,12 +20,9 @@ import org.apache.lucene.analysis.miscellaneous.SetKeywordMarkerFilter;
 import org.apache.lucene.analysis.standard.StandardTokenizer;
 
 /**
- * {@link CustomAnalyzer} that includes logic from StandardAnalyzer and EnglishAnalyzer.
+ * {@priyanawal CustomAnalyzer} that includes logic from StandardAnalyzer and EnglishAnalyzer.
  */
 public final class GeneralisedCustomAnalyzer extends StopwordAnalyzerBase {
-
-  /** An unmodifiable set containing some common English words that are not usually useful
-   for searching.*/
   public static final CharArraySet ENGLISH_STOP_WORDS_SET;
 
   static {
@@ -163,82 +143,35 @@ public final class GeneralisedCustomAnalyzer extends StopwordAnalyzerBase {
     return ENGLISH_STOP_WORDS_SET;
   }
 
-  /**
-   * Set the max allowed token length.  Tokens larger than this will be chopped
-   * up at this token length and emitted as multiple tokens.  If you need to
-   * skip such large tokens, you could increase this max length, and then
-   * use {@code LengthFilter} to remove long tokens.  The default is
-   * {@link StandardAnalyzer#DEFAULT_MAX_TOKEN_LENGTH}.
-   */
   public void setMaxTokenLength(int length) {
     maxTokenLength = length;
   }
-    
-  /** Returns the current maximum token length
-   * 
-   *  @see #setMaxTokenLength */
+ 
   public int getMaxTokenLength() {
     return maxTokenLength;
   }
   
-  /**
-   * Set the min allowed token length.  Tokens smaller than this will be removed
-   * through the use of {@code LengthFilter}.  The default is
-   * {@link StandardAnalyzer#DEFAULT_MIN_TOKEN_LENGTH}.
-   */
   public void setMinTokenLength(int length) {
     minTokenLength = length;
   }
-    
-  /** Returns the current maximum token length
-   * 
-   *  @see #setMinTokenLength */
+   
   public int getMinTokenLength() {
     return minTokenLength;
   }
   
-  /**
-   * Builds an analyzer with the default stop words: {@link #getDefaultStopSet}.
-   */
   public GeneralisedCustomAnalyzer() {
     this(ENGLISH_STOP_WORDS_SET);
   }
   
-  /**
-   * Builds an analyzer with the given stop words.
-   * 
-   * @param stopwords a stopword set
-   */
   public GeneralisedCustomAnalyzer(CharArraySet stopwords) {
     this(stopwords, CharArraySet.EMPTY_SET);
   }
 
-  /**
-   * Builds an analyzer with the given stop words. If a non-empty stem exclusion set is
-   * provided this analyzer will add a {@link SetKeywordMarkerFilter} before
-   * stemming.
-   * 
-   * @param stopwords a stopword set
-   * @param stemExclusionSet a set of terms not to be stemmed
-   */
   public GeneralisedCustomAnalyzer(CharArraySet stopwords, CharArraySet stemExclusionSet) {
     super(stopwords);
     this.stemExclusionSet = CharArraySet.unmodifiableSet(CharArraySet.copy(stemExclusionSet));
   }
 
-  /**
-   * Creates a
-   * {@link org.apache.lucene.analysis.Analyzer.TokenStreamComponents}
-   * which tokenizes all the text in the provided {@link Reader}.
-   * 
-   * @return A
-   *         {@link org.apache.lucene.analysis.Analyzer.TokenStreamComponents}
-   *         built from an {@link StandardTokenizer} filtered with
-   *         {@link EnglishPossessiveFilter},
-   *         {@link LowerCaseFilter}, {@link StopFilter}
-   *         , {@link SetKeywordMarkerFilter} if a stem exclusion set is
-   *         provided and {@link PorterStemFilter}.
-   */
   @Override
   protected TokenStreamComponents createComponents(String fieldName) {
     final StandardTokenizer source = new StandardTokenizer();
